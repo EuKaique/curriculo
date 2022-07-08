5090a45b7582c3214713067178e8488aea9840e3
Kaique <kaiqueols75@gmail.com>
2022-07-08 16:48:49 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,6 +14,7 @@ const cores = (toggleId, novaCor) => {
 }
 cores('alterar-cores','body');
 cores('alterar-cores','header');
+cores('alterar-cores','a');
 
 /* MOSTRAR MENU */
 const showMenu = (toggleId, navId) => {
