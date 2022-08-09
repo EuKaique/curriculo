df807f4482f7e8c5f6a65614842f00cd69635224
Kaique <kaiqueols75@gmail.com>
2022-08-09 13:02:19 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,91 +1,111 @@
-/* ALTERAR CORES */
-const cores = (toggleId, novaCor) => {
-    const toggle = document.getElementById(toggleId),
-    corBody = document.querySelector(novaCor);
-    /*
-    corA = document.querySelector(novaCor);
-    corH3 = document.querySelector(novaCor);
-    corH4 = document.querySelector(novaCor);
-    corSpan = document.querySelector(novaCor);
-    corP = document.querySelector(novaCor);
-    corLink = document.querySelector(novaCor);
-    */
+import { html2pdf } from "./html2pdf.bundle.min";
+
+// MOSTRAR MENU
+    const showMenu = (toggleId, navId) => {
+        const toggle = document.getElementById(toggleId),
+              nav    = document.getElementById(navId)
+
+        //Validando as variáveis existentes
+        if(toggle && nav){
+            toggle.addEventListener('click', () => {
+                // Nós adicionamos a classe show-menu para a tag div com a classe nav__menu
+                nav.classList.toggle('show-menu')
+            })
+        }      
+    }
+    showMenu('nav-toggle', 'nav-menu')
+// REMOVER MENU
+    const navLink = document.querySelectorAll('.nav__link')
+
+    function linkAction(){
+        const navMenu = document.getElementById('nav-menu')
+        // Quando nós clicamos em cada nav__link, nós removemos a classe show-menu
+        navMenu.classList.remove('show-menu')
+    }
+    navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// SCROLL DE SEÇÃO ATIVA LINK
+    const sections = document.querySelectorAll('section[id]')
+
+    function scrollActive(){
+        const scrollY = window.pageYOffset
+
+        sections.forEach(current =>{
+            const sectionHeight = current.offsetHeight
+            const sectionTop = current.offsetTop - 50;
+            sectionId = current.getAttribute('id')
+
+            if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+                document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+            }else{
+                document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+            }
+        })
+    }
+    window.addEventListener('scroll', scrollActive)
+
+// MOSTRAR SCROLL TOP
+    function scrollTop(){
+        const scrollTop = document.getElementById('scroll-top');
+        // Quando a barra de scroll está com 560 da tela, adiciona a classe show-scroll com a classe scroll-top
+        if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+    }
+    window.addEventListener('scroll', scrollTop)
+
+// MUDAR TEMA - BRANCO OU PRETO
+    const themeButton = document.getElementById('theme-button')
+    const darkTheme = 'dark-theme'
+    const iconTheme = 'bx-sun'
+
+    // Tópico selecionado anteriormente (se o usuário for selecionado)
+    const selectedTheme = localStorage.getItem('selected-theme')
+    const selectedIcon = localStorage.getItem('selected-icon')
+
+    // Obtemos o tema atual que a interface possui validando a classe dark-theme
+    const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+    const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+
+    // Validamos se o usuário já escolheu um tópico
+    if (selectedTheme) {
+    // Se a validação for cumprida, perguntamos qual foi o problema para saber se ativamos ou desativamos o escuro
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+    }
+
+    // Ative/desative o tema manualmente com o botão
+    themeButton.addEventListener('click', () => {
+        // Adicione ou remova o tema escuro / ícone
+        document.body.classList.toggle(darkTheme)
+        themeButton.classList.toggle(iconTheme)
+        // Salvamos o tema e o ícone atual que o usuário escolheu
+        localStorage.setItem('selected-theme', getCurrentTheme())
+        localStorage.setItem('selected-icon', getCurrentIcon())
+    })
+
+// REDUZIR A TELA DE IMPRIMIR PARA O FORMATO A4
+    function scaleCv(){
+        document.body.classList.add('scale-cv');
+    }
+
+// REDUZIR O TAMANHO QUANDO O CV FOR BAIXADO
+    function removeScale(){
+        document.body.classList.remove('scale-cv');
+    }
+
+// GERAR O PDF
+    let areaCv       = document.getElementById('area-cv')
+    let resumeButton = document.getElementById('resume-button');
+
+    //função que chama areaCv e as opções do Html2Pdf
+    function generateResume(){
+        html2pdf(areaCv);
+    }
     
-    if(toggle && corBody){
-        toggle.addEventListener('click', () => {
-            corBody.classList.toggle('inverter');
-            /*
-            corA.classList.toggle('inverter');
-            corH3.classList.toggle('inverter');
-            corH4.classList.toggle('inverter');
-            corSpan.classList.toggle('inverter');
-            corP.classList.toggle('inverter');
-            corLink.classList.toggle('inverter');
-            */
-        });
-    }
+    //Quando  o botão for clicado, é executada essas 3 funções
+    resumeButton.addEventListener('click', () => {
+        // 1. A classe scale-cv é adicionada, ela reduz a tela para impressão
+        scaleCv();
 
-}
-cores('alterar-cores','body');
-cores('alterar-cores','header');
-
-/* MOSTRAR MENU */
-const showMenu = (toggleId, navId) => {
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId);
-
-    if(toggle && nav){
-        toggle.addEventListener('click', () => {
-            nav.classList.toggle('show');
-        });
-    }
-}
-showMenu('nav-toggle','nav-menu');
-
-/* ATIVAR E REMOVER MENU */
-const navLink = document.querySelectorAll('.nav__link');
-
-function linkAction(){
-    // Link ativo
-    navLink.forEach(n => n.classList.remove('active'));
-    this.classList.add('active');
-    // Remover o menu no smartphone
-    const navMenu = document.getElementById('nav-menu');
-    navMenu.classList.remove('show');
-}
-
-navLink.forEach(n => n.addEventListener('click', linkAction));
-
-/* SCROLL REVEAL ANIMATION */
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
-    duration: 2000,
-    reset: true
-});
-
-/* SCROLL HOME */
-sr.reveal('.home__title',{});
-sr.reveal('.button',{delay: 200});
-sr.reveal('.home__img',{delay: 400});
-sr.reveal('.home__social-icon',{interval: 200});
-
-/* SCROLL ABOUT */
-sr.reveal('.about__img',{});
-sr.reveal('.about__subtitle',{delay: 200});
-sr.reveal('.about__text',{delay: 400});
-
-/* SCROLL HABILIDADES */
-sr.reveal('.skills__subtitle',{});
-sr.reveal('.skills__text',{delay: 200});
-sr.reveal('.skills__data',{interval: 200});
-sr.reveal('.skills__img',{delay: 400});
-
-/* SCROLL PORTFOLIO */
-sr.reveal('.work__img',{interval: 200});
-
-/* SCROLL EXPERIÊNCIA */
-sr.reveal('.experience__container',{interval: 200});
-
-/* SCROLL CURSOS */
-sr.reveal('.course__container',{interval: 200});
+        // 2. O PDF é criado
+        generateResume();
+    });
